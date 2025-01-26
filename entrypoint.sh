90bcb23c8ea6347f275c2925e0b515c8587a1818
Sam W <sam@wlcx.cc>
2025-01-26 16:05:08 +0000

    Don't try to set anonymous permission

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 mc alias set deploy $MINIO_ENDPOINT $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
 
-mc mirror --remove --overwrite $1 "deploy/$2" && mc anonymous set public "deploy/$2"
+mc mirror --remove --overwrite $1 "deploy/$2"
